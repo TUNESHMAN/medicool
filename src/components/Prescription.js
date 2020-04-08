0df00a236a5bb151c1456411484c25ee4dbb9693
Babatunde <babatundea15@gmail.com>
2020-04-08 14:29:37 +0100

    delete function

--- src/components/Prescription.js
+++ src/components/Prescription.js
@@ -1,47 +1,51 @@
 import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
-import { getPrescription } from "../state/actions/drugAction";
-import { Card, Avatar } from 'antd';
+import { getPrescription, deletePrescription } from "../state/actions/drugAction";
+import { Card, Avatar } from "antd";
 import logo from "../images/mediool.png";
-import { PlusOutlined, EyeOutlined, DeleteOutlined } from '@ant-design/icons';
+import { PlusOutlined, EyeOutlined, DeleteOutlined } from "@ant-design/icons";
 // import "./form.css";
 
 const { Meta } = Card;
 
 function Prescription(props) {
   console.log(props);
-//   const [loading, setLoading] = useState(true)
+  //   const [loading, setLoading] = useState(true)
 
-//  const  onChange = checked=>{
-//     setLoading({loading: !checked})
-//   }
+  //  const  onChange = checked=>{
+  //     setLoading({loading: !checked})
+  //   }
+
+  const handleDelete = (_id) => {
+    props.deletePrescription(_id);
+  };
   useEffect(() => {
     props.getPrescription();
   }, []);
 
- 
   return (
     <div>
       <h1>Prescription</h1>
 
       <div>
-      {/* <Switch checked={!loading} onChange={onChange} /> */}
+        {/* <Switch checked={!loading} onChange={onChange} /> */}
         {props.prescription.map((med, index) => (
           <Card
             style={{ width: 300, marginTop: 16 }}
             actions={[
               <PlusOutlined key="plus" />,
               <EyeOutlined key="eye" />,
-              <DeleteOutlined key="delete" />
+              <DeleteOutlined
+                key="delete"
+                onClick={() => handleDelete(med._id)}
+              />,
             ]}
           >
-           
-              <Meta
-                avatar={<Avatar src={logo} />}
-                title={med.drug}
-                description={med.unit}
-              />
-          
+            <Meta
+              avatar={<Avatar src={logo} />}
+              title={med.drug}
+              description={med.unit}
+            />
           </Card>
         ))}
       </div>
@@ -49,8 +53,8 @@ function Prescription(props) {
   );
 }
 
-const mapStateToProps = state => ({
-  prescription: state.prescription.drugs
+const mapStateToProps = (state) => ({
+  prescription: state.prescription.drugs,
 });
 
-export default connect(mapStateToProps, { getPrescription })(Prescription);
+export default connect(mapStateToProps, { getPrescription, deletePrescription })(Prescription);
