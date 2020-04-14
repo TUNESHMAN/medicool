4cf81725495ecc74431f7eea5580092b94c01286
Babatunde <babatundea15@gmail.com>
2020-04-14 11:56:44 +0100

    styled the homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,128 +5,129 @@ import background from "../images/background.jpg";
 import capsule from "../images/capsule.jpg";
 import syrup from "../images/syrup.jpg";
 import antibiotics from "../images/antibiotics.jpg";
+import solution from "../images/solution.jpg";
 import antiseptics from "../images/antiseptics.jpg";
 import reminder from "../images/reminder.jpg";
+import Injections from "../images/Injections.jpg";
+import Firstaid from "../images/Firstaid.jpg";
+import Disinfectant from "../images/Disinfectant.jpg";
+import { Link } from "react-router-dom";
 function Home() {
   return (
     <div>
       <Navbar />
 
       <div
-        class="background"
+        className="background"
         style={{
           backgroundImage: `url(${background})`,
         }}
       ></div>
-      <div class="header-text">
-        <p class="quick-header">
+      <div className="header-text">
+        <p className="quick-header">
           <strong>Welcome to Medicool</strong>
         </p>
-        <p class="sub-text">
-          <strong>Get your kids working</strong>
+        <p className="sub-text">
+          <strong>Keep track of your medications</strong>
         </p>
 
         <div id="buttons">
+          <Link className="start-btn transparent" to="/login">
+            SIGN-IN
+          </Link>
+
           <a
-            class="start-btn"
-            href="https://objective-dubinsky-42e588.netlify.com/signup"
+            className="start-btn transparent"
+            href="about.html"
             target="_blank"
           >
-            SIGN-UP
-          </a>
-          <a class="start-btn transparent" href="about.html" target="_blank">
             LEARN MORE
           </a>
         </div>
       </div>
 
-      {/* INFO ABOUT MINECHORE */}
+      {/* INFO ABOUT MEDICOOL */}
 
-      <section class="second-section">
-        <div class="about-section">
+      <section className="second-section">
+        <div className="about-section">
           <img
-            class="about-section3"
+            className="about-section3"
             src="./Images/adult-black-and-white-child-736428.jpg"
             alt=""
           />
-          <p class="para">
-            Every member of your household can make use of the MineChore app. If
-            there are chores for your kids to do, the MineChore app is a great
-            way to keep track of events. Parents only need to sign up on the
-            application. Adding a child's account isn't difficult too. This
-            enables a child to sign in and submit a completed chore before it is
-            confirmed by a parent.
+          <p className="para">
+            Everyone can make use of the MediCool app. If there are drugs for
+            you to take, the MediCool app is a great way to keep track of your
+            drug usage. You only need to sign up on the application. Adding a
+            prescription and usage formula isn't difficult too.
           </p>
         </div>
-        <div class="right-section">
-          <h2>CREATE AND ASSIGN CHORES</h2>
+        <div className="right-section">
+          <h2>CREATE PRESCRIPTIONS AND ADD USAGE FORMULA</h2>
           <p>
-            With MineChore, you can easily create and assign tasks for everyone.
-            Also, it becomes easier to draw up a schedule. This schedule
-            reflects on the dashboard of every child. Once a child confirms that
-            a chore has been done, the parent must approve. It is only after
-            approval that credits are awarded.
+            With MediCool, you can easily create and views drug usage formula.
+            Also, it is very easy to see all prescriptions and formula. All you
+            need do is login to your dashboard.
           </p>
-          <p class="earn">Create a chore</p>
+          <p className="earn">Create a Prescription</p>
         </div>
       </section>
 
       {/* SECOND INFO SECTION */}
 
-      <section class="third-section">
-        <div class="about-section">
-          <img class="about-section2" src={reminder} alt="" />
+      <section className="third-section">
+        <div className="about-section">
+          <img className="about-section2" src={reminder} alt="" />
         </div>
-        <div class="right-section">
+        <div className="right-section">
           <h2>BE REMINDED OF YOUR MEDICAL PLAN</h2>
           <p>
-            Medicool is a good way to teach your kids about savings. Work done
-            equals money earned. With MineChore, it's easy to get your kids
-            excited about chores with our "gamified" approach to household chore
-            tracking and management.
+            MediCool is a good way to learn how to take your drugs religiously.
+            With this app, it becomes easier to stick to whatever plan our
+            doctor has given to you. 
           </p>
-          <p class="earn">Check Dashboard</p>
+          <p className="earn">Check Dashboard</p>
         </div>
       </section>
 
-      {/* CHORES LISTED HERE */}
+      {/* DRUGS LISTED HERE */}
 
-      <div class="list-items">
-        <div class="tools">
-          <img class="profile-img" src={antibiotics} alt="Chore" />
-          <p class="txt">Antibiotics</p>
-          <p class="price">Points: 5</p>
+      <div className="list-items">
+        <div className="tools">
+          <img className="profile-img" src={antibiotics} alt="Chore" />
+          <p className="txt">Antibiotics</p>
+          <p className="price">Points: 5</p>
         </div>
 
-        {/* <div class="tools">
+         <div class="tools">
           <img
             class="profile-img"
-            src="./Images/garden-gardening-grass-589.jpg"
+            src={solution}
             alt="Tool"
           />
-          <p class="txt">Mowing</p>
+          <p class="txt">Solution</p>
           <p class="price">Points: 10</p>
-        </div> */}
-        {/* 
+        </div> 
+        
         <div class="tools">
           <img
             class="profile-img"
-            src="./Images/blurred-background-close-up-clothes-2517866.jpg"
+            src={Injections}
             alt="Chore"
           />
-          <p class="txt">Laundry</p>
+          <p class="txt">Injection</p>
           <p class="price">Points: 25</p>
-        </div> */}
+        </div> 
 
-        {/* <div class="tools">
+         <div class="tools">
           <img
             class="profile-img"
-            src="./Images/car-cleaning-washing-5989.jpg"
+            src={Disinfectant}
             alt="Chore"
           />
-          <p class="txt">Wash Car</p>
+          <p class="txt">Disinfectant</p>
           <p class="price">Points: 15</p>
-        </div> */}
+        </div> 
 
         <div class="list-items2">
           <div class="tools">
@@ -147,15 +148,15 @@ function Home() {
             <p class="price">Points: 7</p>
           </div>
 
-          {/* <div class="tools">
+          <div class="tools">
             <img
               class="profile-img"
-              src="./Images/animal-animal-lover-animal-photography-1981111.jpg"
+              src={Firstaid}
               alt="Chore"
             />
-            <p class="txt">Feed Pets</p>
+            <p class="txt">First Aid</p>
             <p class="price">Points: 5</p>
-          </div> */}
+          </div> 
         </div>
       </div>
 
