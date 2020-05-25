621166c15213f85a539586a4b9190530e786a8db
Babatunde <babatundea15@gmail.com>
2020-05-25 13:02:54 +0100

    I styled the no prescription message

--- src/components/Prescription.js
+++ src/components/Prescription.js
@@ -88,7 +88,9 @@ function Prescription(props) {
             <Spin size="large" spinning={props.isFetching} />
           </div>
         ) : props.prescription.length === 0 ? (
-          <h2>There are no prescriptions</h2>
+          <div className="heading">
+            <h2>There are no prescriptions</h2>
+          </div>
         ) : (
           <div className="prescription-card">
             {props.prescription.map((med) => (
@@ -139,61 +141,7 @@ function Prescription(props) {
             ))}
           </div>
         )}
-        {/* {props.isFetching && (
-          <div className="spinner">
-            <Spin size="large" spinning={props.isFetching} />
-          </div>
-        )}
-        {props.prescription && !props.isFetching && (
-          <div className="prescription-card">
-            {props.prescription.map((med) => (
-              <div className="card">
-                <Card
-                  key={med.id}
-                  data-test-id="prescription-card"
-                  style={{
-                    width: 300,
-                    marginBottom: "12px",
-                    borderRadius: "12px",
-                  }}
-                  actions={[
-                    <PlusOutlined
-                      key="plus"
-                      onClick={() => toggleFormula(med._id)}
-                    />,
 
-                    <EyeOutlined
-                      key="eye"
-                      onClick={() => handleView(med._id)}
-                    />,
-                    <DeleteOutlined
-                      key="delete"
-                      onClick={() => handleDelete(med._id)}
-                    />,
-                  ]}
-                >
-                  <p>
-                    <span className="card-info">Drug Name :</span>
-                    <span className="card-details"> {med.drug}</span>
-                  </p>
-
-                  <p>
-                    <span className="card-info">Drug Count : </span>
-                    <span className="card-details"> {med.unit}</span>
-                  </p>
-                  <p>
-                    <span className="card-info">Start Date :</span>
-                    <span className="card-details"> {med.start_Date}</span>
-                  </p>
-                  <p>
-                    <span className="card-info">End Date :</span>
-                    <span className="card-details"> {med.end_Date}</span>
-                  </p>
-                </Card>
-              </div>
-            ))}
-          </div>
-        )} */}
         <div>
           <Modal
             title="Have a new formula?"
